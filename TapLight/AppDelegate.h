94a6183d61eff6a4f90cfc3baa00b229f16b92db
Andrew Schumacher <Andrew@s211n143.resnet.geneseo.edu>
2012-02-09 16:04:26 -0500

    Initial Commit

---
+++ TapLight/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TapLight
+//
+//  Created by Andrew Schumacher on 2/9/12.
+//  Copyright (c) 2012 XeSys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TapLight/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TapLight
+//
+//  Created by Andrew Schumacher on 2/9/12.
+//  Copyright (c) 2012 XeSys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
